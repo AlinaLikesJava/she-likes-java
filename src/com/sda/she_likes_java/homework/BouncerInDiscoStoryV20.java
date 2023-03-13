7042972e9d956fdd8d1e233e26f7c6ed4ca3ef8b
Alina <anisimovaalina@gmail.com>
2023-03-13 18:39:55 +0200

    homework 13.03.23 2

--- src/com/sda/she_likes_java/homework/BouncerInDiscoStoryV20.java
+++ src/com/sda/she_likes_java/homework/BouncerInDiscoStoryV20.java
@@ -2,7 +2,7 @@ package com.sda.she_likes_java.homework;
 
 import java.util.Scanner;
 
-public class BouncerInDiscoStory {
+public class BouncerInDiscoStoryV20 {
     public static void main(String[] args) {
         System.out.println("The party begins, a lot of people are coming to the club");
 
@@ -24,23 +24,20 @@ public class BouncerInDiscoStory {
             System.out.println("Welcome to the club!");
         } else {
             System.out.println(declineMessage);
-        }
-        if (!isAllowedToParty) {
             System.out.println("The people try again. They come up to the old bouncer");
             System.out.println("The old bouncer asks,");
-            System.out.println("Hi, how old are you? ");}
+            System.out.println("Hi, how old are you? ");
+        int age2 = inputReader.nextInt();
+        System.out.println("What is your height? ");
+        int height2 = inputReader.nextInt();
+        System.out.println("Can you sing well? ");
+        boolean canSing2 = inputReader.nextBoolean();
 
-            int age2 = inputReader.nextInt();
-            System.out.println("What is your height? ");
-            int height2 = inputReader.nextInt();
-            System.out.println("Can you sing well? ");
-            boolean canSing2 = inputReader.nextBoolean();
+        boolean isAllowedToParty2 = age2 >= 18 || height2 >= 190 || canSing2;
 
-            boolean isAllowedToParty2 = age2 >= 18 || height2 >= 190 || canSing2;
-
-            if (isAllowedToParty2) {
-                System.out.println("Welcome to the club!");
-            } else {
-                System.out.println(declineMessage);
-            }
-        }}
+        if (isAllowedToParty2) {
+            System.out.println("Welcome to the club!");
+        } else {
+            System.out.println(declineMessage);
+        }
+    }}}
