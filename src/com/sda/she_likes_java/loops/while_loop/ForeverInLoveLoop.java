5288dd1a9867145097f536a5c88519abdbbfb9dd
Alina <anisimovaalina@gmail.com>
2023-03-15 21:21:27 +0200

    ability to break the instruction

---
+++ src/com/sda/she_likes_java/loops/while_loop/ForeverInLoveLoop.java
@@ -0,0 +1,21 @@
+package com.sda.she_likes_java.loops.while_loop;
+
+public class ForeverInLoveLoop {
+
+    public static void main(String[] args) {
+        int numberOfTrials = 0;
+        while(true){
+            System.out.println("Forever");
+            numberOfTrials++;
+
+            if (numberOfTrials >= 10) {
+               break;
+            }
+
+            }
+
+
+        }
+
+    }
+
