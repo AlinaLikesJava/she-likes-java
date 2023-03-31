0cec523fd571095685df71eddf1e9e9d7976796f
Alina <anisimovaalina@gmail.com>
2023-03-31 20:14:33 +0300

    House example - with shared house object

--- src/com/sda/she_likes_java/objects/HouseExample.java
+++ src/com/sda/she_likes_java/objects/HouseExample.java
@@ -3,17 +3,28 @@ package com.sda.she_likes_java.objects;
 public class HouseExample {
 
     public static void main(String[] args) {
-        House myHouse = new House (4, 2);
-        System.out.println("I've got a new house " + myHouse);
+        House myHouse = new House(4, 2);
+        System.out.println("I've got new house: " + myHouse);
 
-        System.out.println("Currently my house has " + myHouse.getNumberOfRooms() + " rooms");
-
-        System.out.println("I need more rooms");
+        System.out.println("Currently my house has got " + myHouse.getNumberOfRooms() + " rooms");
+        System.out.println("I need more rooms...");
         myHouse.setNumberOfRooms(5);
 
-        System.out.println("Hey, I've got more rooms now" );
+        System.out.println("Hey, I've got more rooms now...");
         System.out.println("Currently my house has got " + myHouse.getNumberOfRooms() + " rooms");
 
-    }
+        System.out.println("=======================================");
+        HouseOwner wife = new HouseOwner("Mary", myHouse);
+        HouseOwner husband = new HouseOwner("Jack", myHouse);
 
+        System.out.println("Mary would like to repair her house");
+        wife.getHouse().setNumberOfBathrooms(3); // House house = wife.getHouse(); house.setNumberOfBathrooms(3);
+        System.out.println("Currently the house looks like this: " + myHouse);
+
+        husband.getHouse().setNumberOfBathrooms(1);
+        System.out.println("Currently the house looks like this: " + myHouse);
+
+        System.out.println("Mary: " + wife);
+        System.out.println("Jack: " + husband);
+    }
 }
